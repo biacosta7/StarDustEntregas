d36cf3a5c89822ac5ba57c8e2600000262f471cb
Thiagovbrayner <tvvb@cesar.school>
2024-06-18 17:14:12 -0300

    Jogo de Adivinhação

---
+++ Adivinhação/Jogo Adivinhação/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(){
+
+        Random random = new Random();
+        int aleatorio = random.Next(0,20);
+        Console.WriteLine(aleatorio);
+
+        Console.Write($"Tente acertar um número entre 0 e 20 (3 chances) : ");
+        int num = Convert.ToInt16(Console.ReadLine());
+
+        for (int i=0; i<2; i++){
+            if (num == aleatorio){
+
+                Console.WriteLine($"Você acertou! O número era {num}");
+                break;
+                
+            }
+            else{
+                Console.Write("Errou! Tente outro número : ");
+                num = Convert.ToInt16(Console.ReadLine());
+                if (num == aleatorio){
+                    Console.WriteLine($"Você acertou! O número era {num}");
+                 break;
+                }
+            }
+        }
+        Console.Write("Acabou!");
+
+        
+
+    }
+}
